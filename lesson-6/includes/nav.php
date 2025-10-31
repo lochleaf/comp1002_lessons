d391295f7a8f6d47a97fb5a974965bb591ad9521
lochleaf <200536258@student.georgianc.on.ca>
2025-10-31 16:37:45 -0400

    added lab-c an finish lesson8

---
+++ lesson-6/includes/nav.php
@@ -0,0 +1,12 @@
+		<!-- get the global nav HTML -->
+            
+            <nav>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="red.php">Red blocks</a></li>
+					<li><a href="blue.php">Blue blocks</a></li>
+					<li><a href="yellow.php">Yellow blocks</a></li>
+				</ul>
+			</nav>
+
+            <!-- get the global nav HTML add the absolute url website link that connects to lessons or check video if you dont understand u can also shorten it the link-->
